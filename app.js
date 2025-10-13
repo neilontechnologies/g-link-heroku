3dca75f9eb839553b8362bf5bcf186ccca3f3058
Neilon Technologies LLP <82505395+neilontechnologies@users.noreply.github.com>
2025-10-13 18:07:33 +0530

    Added the logs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 // Use to authenticate heroku access key
 app.use((req, res, next) => {
-	console.log('HII');
+	console.log('HII123');
   const apiKey = process.env.API_KEY;
   const decryptedPayload = decryptAES256(req.body, apiKey.substring(0, 32));
   const salesforceAuthenticationInfo = JSON.parse(decryptedPayload);
