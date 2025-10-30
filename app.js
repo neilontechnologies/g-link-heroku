2b0743190e9490b631f76fb0e9d765cc5f502f0f
Neilon Technologies LLP <82505395+neilontechnologies@users.noreply.github.com>
2025-10-30 15:13:27 +0530

    Added support to work with open payload

--- app.js
+++ app.js
@@ -22,12 +22,9 @@ const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 // Use to authenticate heroku access key
 app.use((req, res, next) => {
-  console.log('HIIII');
   const contentType = req.headers['content-type'];
-  console.log(contentType);
   let salesforceAuthenticationInfo;
   const apiKey = process.env.API_KEY;
-  console.log(process.env.API_KEY);
   if(contentType == 'text/plain'){
     const decryptedPayload = decryptAES256(req.body, apiKey.substring(0, 32));
     salesforceAuthenticationInfo = JSON.parse(decryptedPayload);
